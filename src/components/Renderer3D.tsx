e1742fef859a1f3ea81f1264bff9577a7ebb1e9e
lyukidon <qgx99098@gmail.com>
2024-07-25 01:50:39 +0900

    [feat] 평면도 3d 랜더링

--- src/components/Renderer3D.tsx
+++ src/components/Renderer3D.tsx
@@ -1,25 +1,70 @@
-import React, { DetailedHTMLProps, HTMLAttributes, useEffect, useRef } from "react";
-import * as THREE from "three";
-import { render3d } from "../utils/render3d";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { Mesh, PlaneGeometry } from "three";
+import { Canvas, ThreeElements, useFrame, useThree } from "@react-three/fiber";
+import { StateType } from "../store/store.type";
 import { useSelector } from "react-redux";
-import {StateType} from '../store/store.type'
+import { OrbitControls } from '@react-three/drei'
 
-function Renderer3D() {
-  const imgData = useSelector((state: StateType) => state.imgData)
-  const containerRef = useRef<HTMLDivElement>(null);
+function terrainHeightMap(imageData: ImageData){
+  const width = imageData.width;
+  const height = imageData.height;
+  const geometry = new PlaneGeometry(width, height, width - 1, height - 1);
 
-  useEffect(() => {
-    if (containerRef.current !== null){
-      const renderer = new THREE.WebGLRenderer();
-      renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
-      renderer.setClearColor(0xffffff, 1);
-      containerRef.current.appendChild(renderer.domElement);
+  for (let i = 0;  i < geometry.attributes.position.count; i++) {
+    const x = i % width;
+    const y = Math.floor(i / width);
+    const pixelIndex = (y * width + x) * 4;
+    const heightValue = imageData.data[pixelIndex] / 255 * 10; // 높이 값을 0에서 10 사이로 정규화
+    // const heightValue: number = 30;
+    geometry.attributes.position.setZ(i, -heightValue);
+  }
 
-      // render3d(imgData, renderer);
-    }
-  }, []);
-
-  return <div ref={containerRef}></div>;
+  geometry.computeVertexNormals();
+  return geometry;
 }
 
-export default React.memo(Renderer3D);
+function Map(props: ThreeElements["mesh"]) {
+  const imgData = useSelector((state: StateType) => state.imgData);
+  const meshRef = useRef<Mesh>(null!);
+
+  useFrame((state, delta) => {
+    // meshRef.current.rotation.x += delta;
+    // meshRef.current.rotation.y += delta;
+  });
+
+  const terrain = useMemo(() => terrainHeightMap(imgData), [imgData]);
+
+  return (
+    <mesh
+      ref={meshRef}
+      geometry={terrain}
+    >
+      {/* <planeGeometry ref={planeRef} args={[width, height]} /> */}
+      <meshStandardMaterial color={0xeeeeee} wireframe={false} />
+    </mesh>
+  );
+}
+
+function CameraController(){
+  const {camera} = useThree();
+  useEffect(() => {
+    camera.position.set(0, 0, 300);
+    camera.lookAt(0,0,0)
+  }, [camera])
+  return null;
+}
+
+function MapRenderer() {  
+  return (
+    <Canvas>
+      <CameraController />
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Map position={[0, 0, 0]} />
+      <OrbitControls />
+    </Canvas>
+  );
+}
+
+export default React.memo(MapRenderer);
